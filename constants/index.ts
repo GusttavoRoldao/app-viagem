15a74b21444aa8ec4682e2708049fd1dd5762053
GusttavoRoldao <gustavoroldao90@gmail.com>
2024-12-29 18:09:00 -0300

    ladin page app de viagem com ia

---
+++ constants/index.ts
@@ -0,0 +1,90 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Início" },
+  { href: "/", key: "how_soltomapa_work", label: "Como Funciona?" },
+  { href: "/", key: "services", label: "Funcionalidades" },
+  { href: "/", key: "pricing", label: "Preços" },
+  { href: "/", key: "contact_us", label: "Fale Conosco" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Mapas sob medida",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "Crie itinerários e mapas personalizados com IA adaptados às suas necessidades, garantindo que sua jornada seja única e eficiente.",
+  },
+  {
+    title: "Criação de Roteiros",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Crie roteiros personalizados para sua viagem com sugestões de lugares para visitar, comer e se hospedar, tudo em um único app.",
+  },
+  {
+    title: "Tecnologia Avançada",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "Utilizamos tecnologia de ponta com IA para facilitar seu planejamento, adaptando tudo para caber no seu bolso e seguir a sua rotina.",
+  },
+  {
+    title: "Destinos que Combinam com Você",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "Explore lugares que refletem seu estilo e interesses, com sugestões adaptadas para criar experiências únicas em cada viagem.",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Saiba Mais",
+    links: [
+      "Sobre o SoltaOMapa",
+      "Notícias",
+      "Sustentabilidade",
+      "Carreiras",
+      "Política de Privacidade",
+      "Fale Conosco",
+    ],
+  },
+  {
+    title: "Nossa Comunidade",
+    links: [
+      "Viajantes SoltaOMapa",
+      "Exploradores ao redor do mundo",
+      "Histórias e Dicas",
+    ],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Fale Conosco",
+  links: [
+    { label: "Telefone", value: "123-456-7890" },
+    { label: "E-mail", value: "contato@soltaomapa.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
+  ],
+};
